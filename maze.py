99ac054e1d1c08c28f94fc774c14a22b8f9375c6
j <47130044+joemeyer1@users.noreply.github.com>
2019-04-02 02:01:46 -0700

    cleaned up maze, added more interesting maze (which algorithm can solve apparently every time without too much computation)

--- maze.py
+++ maze.py
@@ -2,11 +2,41 @@
 import numpy as np
 
 
+
+# init state same for all
+INITIAL_STATE = [(0, 0)]
+
+
 # SMALL_MAZE dimensions: x = 3, y = 3
 
 SMALL_MAZE = [[0,0,0],
 			  [0,0,0],
 			  [0,0,999]]
+
+SMALL_TERMINAL = [(2,2)]
+
+
+
+ONE_PATH_MAZE = \
+		[[0,0, -4, 0, 0, 0, 0],
+		[-1,1, 1, 2, 0, 0, 0],
+		[0, -2, 0, 4, 8, 16,0],
+		[0,  0,-4, 0, 0, 32,0],
+		[0,  0, 0,-8, 0, 64,0],
+		[0,  0, 0, 0,-16, 0,0]]		
+
+def one_path_terminal():
+	# For one-path, most states are terminal
+	# so compile list of all states
+	one_path_term = [(x, y) for x in range(len(ONE_PATH_MAZE[0])) for y in range(len(ONE_PATH_MAZE))]
+	# then remove the non-terminal ones
+	path_non_term = [(0,0), (1,0), (1, 1), (2, 1), (3, 1), (2,2), (3, 2), (4, 2), (5, 2), (5, 3)]
+	for nont in path_non_term:
+		one_path_term.remove(nont)
+	return one_path_term
+
+ONE_PATH_TERMINAL = one_path_terminal()
+
 
 
 # MAZE/HARD_MAZE dimensions: x = 9, y = 9
@@ -33,28 +63,23 @@ HARD_MAZE = [[0, -10, -10, -1, 0, 10, 50, 100, 500],
 			[0,     0,  -10,  0, 0,  0,  0,   0,   0],
 			[0,     0,   0,  10, 0,  0,  0,   0,   0]]
 
-
-# init state same for all
-INITIAL_STATE = (0, 0)
-SMALL_TERMINAL = (2,2)
 # maze terminal state same for MAZE, HARD_MAZE
-MAZE_TERMINAL = (7, 6)
+MAZE_TERMINAL = [(7, 6)]
 
 
-# start: (0, 0)
-# end:   (7, 6)
 
-MAZE_TERMINAL = (7, 6)
+# data just compiles the relevant info
+SMALL_MAZE_DATA = 	(SMALL_MAZE, 	INITIAL_STATE,	SMALL_TERMINAL)
+ONE_PATH_DATA = 	(ONE_PATH_MAZE, INITIAL_STATE,	ONE_PATH_TERMINAL)
+MAZE_DATA = 		(MAZE, 			INITIAL_STATE,	MAZE_TERMINAL)
+HARD_MAZE_DATA = 	(HARD_MAZE, 	INITIAL_STATE,	MAZE_TERMINAL)
 
 
-maze = SMALL_MAZE
-terminal_state = SMALL_TERMINAL
+
 
 class Maze:
-	def __init__(self, maze = maze, init_states = [INITIAL_STATE], end_state = terminal_state):
-		self.maze = maze
-		self.init_states = init_states
-		self.end_states = [end_state]
+	def __init__(self, data = ONE_PATH_DATA):
+		self.maze, self.init_states, self.end_states = data
 
 
 	def state_space(self, initial=False, non_terminal=False):
@@ -91,5 +116,29 @@ class Maze:
 
 	def terminal_state(self, state):
 		return state in self.end_states
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
