78803dc21ee32dcc1d640bf0723bc3228ffe7f5a
j <47130044+joemeyer1@users.noreply.github.com>
2019-04-25 19:12:45 -0700

    found better parameters (almost always clears at least 1 line now, often clears 5)

--- tetris_test.py
+++ tetris_test.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 25 17:38:46 2019
 
-@author: joe
-"""
 
 from tetris_interface import TetrFace
 from policy import Policy
@@ -17,13 +11,13 @@ def main(visualize=False):
 	# make policy
 	D_in = len(mdp.get_state())
 	D_out = 4
-	H = 1000
+	H = 500
 	M = 50
 	pol = Policy(D_in, D_out, H, M)
-	step_size = .1
-	discount = .9
-	num_episodes=50000
-	max_episode_length=10000
+	step_size = .5
+	discount = .99999999
+	num_episodes=100000
+	max_episode_length=50000
 
 	# make reinforcer
 	r=Reinforcer(mdp, pol, step_size, discount, num_episodes, max_episode_length)
